4b214c06315c96419c9f1454fd63718adabe5437
Ajay Kumar <ajaykumar1071999@gmail.com>
2024-02-25 21:59:45 +0530

    Routes setup

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes  import 
+
+import userRouter from './routes/user.route.js';
+
+//Routes declaration
+
+app.use('/api/v1/users',userRouter)
+
 export { app }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { V2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // file has been uploaded successfully
+
+        console.log('file is uploaded on cloudinary', response.url);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temp files as the upload operation got failed
+
+        return null;
+
+    }
+}
+
+
+export { uploadOnCloudinary }
+
